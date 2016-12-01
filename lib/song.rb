2a69491b49ffc2342312a173bac6767a13824807
Kyle Tulod <tulod.kyle.m@gmail.com>
2016-12-01 12:59:23 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,13 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :filename
+
   @@all = []
+
+  def initialize (name = "", artist_name = "")
+    @name = name
+    @artist_name = artist_name
+    save
+  end
 
   def self.all
     @@all
@@ -9,5 +16,43 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    Song.new
+  end
+
+  def self.new_by_name (name)
+    song = Song.new(name)
+  end
+
+  def self.create_by_name (name)
+    Song.new(name)
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def self.new_from_filename (filename)
+    artist_name, name = filename.gsub(".mp3", ""). split(" - ")
+    Song.new(name, artist_name)
+  end
+  def self.create_from_filename (filename)
+    artist_name, name = filename.gsub(".mp3", ""). split(" - ")
+    Song.new(name, artist_name)
+  end
+
 
 end
